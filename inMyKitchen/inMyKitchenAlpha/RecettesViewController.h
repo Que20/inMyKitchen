6a473ecee741320ce14311b3babb8537a4d94a00
Que20 <maarekkevin@gmail.com>
2013-12-06 00:40:57 +0100

    Support de tous iPhones

---
+++ inMyKitchen/inMyKitchenAlpha/RecettesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecettesViewController.h
+//  inMyKitchen
+//
+//  Created by Kevin Maarek on 28/11/2013.
+//  Copyright (c) 2013 Kevin Maarek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecettesViewController : UIViewController
+
+@end
